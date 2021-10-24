8f01efbd21926fe707938fbd4f53895f432a055e
haxdermak <haxdermak@gmail.com>
2021-10-24 18:29:07 +0200

    225-226

--- Cliente.php
+++ Cliente.php
@@ -1,4 +1,5 @@
 <?php
+include_once "Soporte.php";
 
 class Cliente
 {
@@ -26,14 +27,51 @@ class Cliente
         return $this->numero;
     }
 
-    public function getNumSoporteAlquilados(): int{
+    public function getNumSoporteAlquilados(): int
+    {
         return count($this->soportesAlquilados);
-     }
+    }
 
     //MÉTODOS
 
-    
+    public function tieneAlquilado(Soporte $s): bool
+    {
+        if (array_count_values($this->soportesAlquilados, $s)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
+    public function alquilar(Soporte $s): bool
+    {
+        if ($this->tieneAlquilado($s) == false && $this->getNumSoporteAlquilados() < $this->maxAlquilerConcurrente) {
+            $this->getNumSoporteAlquilados++;
+            echo "El soporte ha sido alquilado a : ".$this->nombre;
+            return true;
+        }
+        return false;
+    }
+
+    public function devolver(int $numSoporte):bool{
+        if(array_count_values($this->soportesAlquilados, $numSoporte)){
+            echo "El soporte con el código ".$numSoporte." ha sido devuelta";
+            $this->getNumSoporteAlquilados--;
+            return true;
+        }{
+            echo "No hay ningún soporte con ese número inscrito en la lista";
+            return false;
+        }
+    }
+
+    public function listaAlquileres(): void{
+
+        echo"El usuario tiene: ".$this->getNumSoporteAlquilados()." alquilados";
+        foreach($this->soportesAlquilados as $s){
+            echo $s->mostrarResumen();
+        }
+
+    }
     public function muestraResumen()
     {
         echo "Nombre: " . $this->nombre . "<br>";
